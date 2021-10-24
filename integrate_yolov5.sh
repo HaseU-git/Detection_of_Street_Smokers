298fa53704b016067987a69c021225282a908ff5
HaseU-git <64348058+HaseU-git@users.noreply.github.com>
2021-10-24 16:22:32 +0900

    Update integrate_yolov5.sh

--- integrate_yolov5.sh
+++ integrate_yolov5.sh
@@ -3,6 +3,7 @@ git clone https://github.com/ultralytics/yolov5.git
 
 mv yolov5/detect.py yolov5/detect_only.py
 mv *.py yolov5
+mv weight.pt yolov5
 
 cd yolov5
 sed -i "s/opencv-python/# opencv-python/g" requirements.txt
